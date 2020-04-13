c9c98e7209a562d724fd9d721e2278d0c2866ae4
NartakarW <jc.sanchez1616@hotmail.com>
2020-04-13 11:59:57 -0500

    front

---
+++ front/build/precache-manifest.8e88332a9840547f969e4151cc919eaf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b81bc548c6987ec5680e588cd4f407a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2c450271373853239d5",
+    "url": "/static/css/main.5c800925.chunk.css"
+  },
+  {
+    "revision": "567d97ae77a6082aa5f3",
+    "url": "/static/js/2.f7965384.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f7965384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2c450271373853239d5",
+    "url": "/static/js/main.6a436d0e.chunk.js"
+  },
+  {
+    "revision": "cd5cc0b019c8434d78aa",
+    "url": "/static/js/runtime-main.95a77404.js"
+  },
+  {
+    "revision": "160e0a5273c4073d8087e7b24adce9f7",
+    "url": "/static/media/deal.160e0a52.svg"
+  },
+  {
+    "revision": "2927f8de4268bae8f11c4dfe3adc72b5",
+    "url": "/static/media/edit.2927f8de.svg"
+  },
+  {
+    "revision": "3a8f8ed27a90bf7b692a963cdc6afaf9",
+    "url": "/static/media/help.3a8f8ed2.png"
+  },
+  {
+    "revision": "911fd45171d74381954df5dfcb9f6d0a",
+    "url": "/static/media/help.911fd451.jpg"
+  },
+  {
+    "revision": "d18f8ff7698cdb1c33f90638159bed87",
+    "url": "/static/media/help1.d18f8ff7.jpg"
+  },
+  {
+    "revision": "2fd68a21635b530c04be82c27d1fc634",
+    "url": "/static/media/new.2fd68a21.svg"
+  }
+]);
